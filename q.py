496ad3a506ea26d0f5b585bd2c8d517d5fd47f21
fedorov alexandr <fedorov4433@mail.ru>
2021-12-20 21:38:38 +0500

    этап 3

--- q.py
+++ q.py
@@ -2,6 +2,9 @@ import sqlite3
 import pandas as pd
 import re
 
+def get_clean_field(field):
+    return re.sub(r'\<[^>]*\>', '', str(field))
+
 con = sqlite3.connect('works.sqlite')
 cursor = con.cursor()
 cursor.execute('drop table if exists works')
@@ -16,35 +19,10 @@ cursor.execute('create table works ('
                'skills TEXT,'
                'otherInfo TEXT)')
 con.commit()
+df = pd.read_csv("works.csv")
 
-cursor.execute('drop table if exists genders')
-cursor.execute('create table genders(id INTEGER PRIMARY KEY AUTOINCREMENT, gender_val TEXT)')
+df.to_sql("works", con, if_exists='append', index=False)
 con.commit()
 
-cursor.execute('INSERT INTO genders(gender_val) SELECT DISTINCT gender FROM works WHERE gender IS NOT NULL')
-con.commit()
-
-cursor.execute('ALTER TABLE works ADD COLUMN gender_id INTEGER REFERENCES genders(id)')
-con.commit()
-
-cursor.execute('UPDATE works SET gender_id = (SELECT id FROM genders WHERE gender_val = works.gender)')
-con.commit()
-
-cursor.execute('ALTER TABLE works DROP COLUMN gender')
-con.commit()
-
-cursor.execute('drop table if exists education')
-cursor.execute('create table education(id INTEGER PRIMARY KEY AUTOINCREMENT, edu_val TEXT)')
-con.commit()
-
-cursor.execute('INSERT INTO education(edu_val) SELECT DISTINCT educationType FROM works WHERE educationType IS NOT NULL')
-con.commit()
-
-cursor.execute('ALTER TABLE works ADD COLUMN educationType_id INTEGER REFERENCES education(id)')
-con.commit()
-
-cursor.execute('UPDATE works SET educationType_id = (SELECT id FROM education WHERE edu_val = works.educationType)')
-con.commit()
-
-cursor.execute('ALTER TABLE works DROP COLUMN educationType')
-con.commit()
+df['skills'] = df['skills'].apply(get_clean_field)
+df['otherInfo'] = df['otherInfo'].apply(get_clean_field)
